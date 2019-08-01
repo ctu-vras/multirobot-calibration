5b0d4ea1536409103e1df4dc0ea64649b5a60417
Jakub Rozlivek <rozlijak@fel.cvut.cz>
2019-08-01 16:15:25 +0200

    repaired rmsErrors

--- Utils/rmsErrors.m
+++ Utils/rmsErrors.m
@@ -36,7 +36,7 @@ function [rms_errors, errors_all] = rmsErrors(dh, robot, datasets, dataset_index
                 rms_distances(rep+(pert_level-1)*optim.repetitions) = sqrt(sum((distances-refDist).^2)/size(distances, 2));
             end
             if(approach.planes)
-                plane_distances = getPlaneDist(dh_pars, robot, dataset.plane, optim);
+                plane_distances = getPlaneDist(dh_pars, robot, dataset.plane);
                 plane_distances_all{end+1} = plane_distances;
                 rms_plane_distances(rep+(pert_level-1)*optim.repetitions) = sqrt(sum(plane_distances.^2)/size(plane_distances, 2));
             end
@@ -46,7 +46,7 @@ function [rms_errors, errors_all] = rmsErrors(dh, robot, datasets, dataset_index
                 rms_dist_from_ext(rep+(pert_level-1)*optim.repetitions) = sqrt(sum(dist_from_ext.^2)/size(dist_from_ext, 2));
             end
             if(approach.eyes)
-                marker_dist = getProjectionDist(dh_pars, robot, dataset.proj, optim);
+                marker_dist = getProjectionDist(dh_pars, robot, dataset.proj);
                 marker_dist_all{end+1} = marker_dist;
                 rms_marker_dist(rep+(pert_level-1)*optim.repetitions) = sqrt(sum(marker_dist.^2)/size(marker_dist, 2));
             end
