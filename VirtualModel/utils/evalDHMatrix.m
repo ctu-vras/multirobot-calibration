94dc199c670eaa767b7274cfea756f4aa19c5af8
Lukáš Rustler <rustlluk@fel.cvut.cz>
2020-09-27 13:40:26 +0200

    Delete VirtualModel

--- VirtualModel/utils/evalDHMatrix.m
+++
@@ -1,8 +0,0 @@
-function G = evalDHMatrix(a, d, alph, thet);
-% Transl (d) along z + rot (thet) about  z
-% Transl (a) along x + rot (alph) about x
-
-G = [       cos(thet), -sin(thet)*cos(alph),  sin(thet)*sin(alph),          cos(thet)*a
-            sin(thet),  cos(thet)*cos(alph), -cos(thet)*sin(alph),          sin(thet)*a
-                    0,            sin(alph),            cos(alph),                    d
-                    0,                    0,                    0,                    1];
